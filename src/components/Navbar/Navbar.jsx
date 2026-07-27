efe95bfc9ccb788fda192843cf24214385ab05ca
Chidiadi Anyanwu <chidiadi01@gmail.com>
2026-07-27 14:36:48 +0100

    Modify the navbar to use CSS modules

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import './Navbar.css';
+import styles from './Navbar.module.css';
 import logoimage from '../../../public/icon-circle.svg'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -12,34 +12,37 @@ const Navbar = () => {
             setIsActive('inactive');
         }
     }
-        return (
-            <div  className={`parent ${isActive}`} >
-                <div className={`nav ${isActive}`}>
-                    <div className='nav-logo desktop'> <img src={logoimage}/> National Trekkers Association</div>
-                    <div className='nav-logo mobile'> <img src={logoimage}/> N T A</div>
-                    <ul className='nav-menu'>
+
+    const stateClass = isActive === 'active' ? styles.active : styles.inactive;
+
+    return (
+            <div className={`${styles.parent} ${stateClass}`}>
+                <div className={`${styles.nav} ${stateClass}`}>
+                    <div className={`${styles['nav-logo']} ${styles.desktop}`}><img src={logoimage} alt="logo" /> National Trekkers Association</div>
+                    <div className={`${styles['nav-logo']} ${styles.mobile}`}><img src={logoimage} alt="logo" /> N T A</div>
+                    <ul className={styles['nav-menu']}>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to="/activities">Activities</Link></li>
                         <li><Link to='/legacies'>LEGacies</Link></li>
                         <li><Link to='/membership'>Membership</Link></li>
-                        <li className='nav-login'><Link to='/login'>Login</Link></li>
+                        <li className={styles['nav-login']}><Link to='/login'>Login</Link></li>
                     </ul>
                 </div>
-                <div className={`ham ${isActive}`} onClick={toggleActive}>
-                                <div className={`bar ${isActive}`}></div>
-                                <div className={`bar ${isActive}`}></div>
-                                <div className={`bar ${isActive}`}></div>
+                <div className={`${styles.ham} ${stateClass}`} onClick={toggleActive}>
+                    <div className={`${styles.bar} ${stateClass}`}></div>
+                    <div className={`${styles.bar} ${stateClass}`}></div>
+                    <div className={`${styles.bar} ${stateClass}`}></div>
                 </div>
-                <div className={`mobile-menu mobile ${isActive}`}>
-                            <div className='menu-items'>
-                                <ul>
-                                    <li><Link to='/'>Home</Link></li>
-                                    <li><Link to="/activities">Activities</Link></li>
-                                    <li><Link to='/legacies'>LEGacies</Link></li>
-                                    <li><Link to='/membership'>Membership</Link></li>
-                                    <li><Link to='login'>Login</Link></li>
-                                </ul>
-                            </div>
+                <div className={`${styles['mobile-menu']} ${styles.mobile} ${stateClass}`}>
+                    <div className={styles['menu-items']}>
+                        <ul>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to="/activities">Activities</Link></li>
+                            <li><Link to='/legacies'>LEGacies</Link></li>
+                            <li><Link to='/membership'>Membership</Link></li>
+                            <li><Link to='login'>Login</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
